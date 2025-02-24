7080f280b37de653eb626796a6495289cb9ea60a
krutzika <kruthika.ramesh99@gmail.com>
2025-02-24 22:10:26 +0000

    Added search_html function to get lyrics when initial url returns 404

--- lyrics_service/main.py
+++ lyrics_service/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, HTTPException, Depends
-from lyrics_service.web_scraper import WebScraper, WebScraperException
+from lyrics_service.web_scraper import LyricsScraper, LyricsScraperException
 from pydantic import BaseModel
 from typing import Annotated
 from lyrics_service.settings import Settings
-from lyrics_service.dependencies import get_settings
+from lyrics_service.dependencies import get_lyrics_scraper
 
 app = FastAPI()
 
@@ -13,12 +13,15 @@ class Lyrics(BaseModel):
     lyrics: str
 
 @app.get('/lyrics', response_model=Lyrics)
-async def get_lyrics(artist: str, track_title: str, settings: Annotated[Settings, Depends(get_settings)]):
+async def get_lyrics(
+    artist: str, 
+    track_title: str, 
+    lyrics_scraper: Annotated[LyricsScraper, Depends(get_lyrics_scraper)]
+):
 
     try: 
-        scraper = WebScraper(base_url=settings.base_url, headers=settings.headers)
-        lyrics =scraper.scrape_lyrics(artist=artist, track_title=track_title)
+        lyrics =lyrics_scraper.scrape_lyrics(artist=artist, track_title=track_title)
         return Lyrics(artist=artist, track_title=track_title, lyrics=lyrics)
     
-    except WebScraperException as e:
+    except LyricsScraperException as e:
         raise HTTPException(status_code=404, detail=str(e))
--- lyrics_service/web_scraper.py
+++ lyrics_service/web_scraper.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import urllib.parse
 
-class WebScraperException(Exception):
+class LyricsScraperException(Exception):
 
     def __init__(self, message: str ):
         super().__init__(message)
 
-class WebScraper:
+class LyricsScraper:
 
     def __init__(self, base_url: str, headers : dict[str,str]):
 
@@ -19,6 +20,23 @@ class WebScraper:
         response = requests.get(url, headers=self.headers)
         response.raise_for_status()
         return response.text
+    
+
+    def _get_lyrics_html(self, artist: str, track_title: str):
+
+        url=self._get_url(artist, track_title)
+        response = requests.get(url, headers=self.headers)
+
+        if response.status_code==200:
+            html=response.text
+
+        elif response.status_code==404:
+            html=self._search_html(artist, track_title)
+        else:
+            raise LyricsScraperException("Lyrics not found")
+        
+        return html
+
 
     def _get_url(self, artist: str, title: str) -> str:
 
@@ -30,17 +48,50 @@ class WebScraper:
         url=f"{self.base_url}/{artist}/{title}.html"
 
         return url
+    
+    def _search_html(self, artist: str, track_title: str):
+
+        url = "https://www.azlyrics.com/"
+        html = self._get_html(url)
+        soup = BeautifulSoup(html, "html.parser")
+        x_selector = soup.select_one("form.navbar-form.navbar-right.search input[name='x']")
+
+        if x_selector is None:
+            raise LyricsScraperException("Search form input 'x' not found")
+        
+        x_value = x_selector.get("value")
+        base_url = "https://search.azlyrics.com/search.php"
+        query_params=urllib.parse.urlencode({"q":f"{artist} {track_title}", "x": x_value})
+        whole_url=f"{base_url}/?{query_params}"
+        top_search = self._get_html(whole_url)
+        new_soup = BeautifulSoup(top_search, "html.parser")
+        title_selector = new_soup.select_one("body > div.container.main-page > div > div > div.panel > table > tbody > tr > td > a")
+
+        if title_selector is None:
+            raise LyricsScraperException("No search results found for the given artist and track title")
+        
+        title_link = title_selector.get("href")
+                
+        lyrics_url = self._get_html(title_link)
+        return lyrics_url
+
+        
+        # get href of top search results 
+        # return html of the href page 
+        
+
 
     def scrape_lyrics(self, artist: str, track_title: str) -> str:
         
         try:
-            url=self._get_url(artist, track_title)
-            html = self._get_html(url)
+            
+            html =self._get_lyrics_html(artist, track_title)
             soup = BeautifulSoup(html, "html.parser")
             lyrics_container = soup.select_one("div.container.main-page div.row div.col-xs-12.col-lg-8.text-center")
             
             if not lyrics_container:
-                raise WebScraperException("Lyrics not found")
+
+                raise LyricsScraperException("Lyrics not found")
 
             lyrics_div = lyrics_container.find_all("div")[5]
             lyrics = lyrics_div.get_text().strip()
@@ -49,4 +100,4 @@ class WebScraper:
 
         except Exception: 
         
-            raise WebScraperException(f"An error occurred while scraping the lyrics")
+            raise LyricsScraperException(f"An error occurred while scraping the lyrics")
