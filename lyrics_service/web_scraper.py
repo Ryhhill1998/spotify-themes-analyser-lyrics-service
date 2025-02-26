4c03d55dabed2638459f283be2c89ce959f6ae0b
krutzika <kruthika.ramesh99@gmail.com>
2025-02-26 21:09:47 +0000

    Changed selector_one to select and added for loop to join all the lines

--- lyrics_service/web_scraper.py
+++ lyrics_service/web_scraper.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 class LyricsScraperException(Exception):
 
     def __init__(self, message: str ):
         super().__init__(message)
+
 
 class LyricsScraper:
 
@@ -13,13 +15,11 @@ class LyricsScraper:
         self.base_url = base_url
         self.headers = headers
 
-    
     def _get_html(self, url: str) -> str:
 
         response = requests.get(url, headers=self.headers)
         response.raise_for_status()
         return response.text
-
 
     def _get_url(self, artist: str, title: str) -> str:
 
@@ -32,25 +32,22 @@ class LyricsScraper:
 
         return url
 
-    
     def scrape_lyrics(self, artist: str, track_title: str) -> str:
         
         try:
             url = self._get_url(artist, track_title)
             html = self._get_html(url)
             soup = BeautifulSoup(html, "html.parser")
-            lyrics_container = soup.select_one("div[data-lyrics-container='true']")
+            lyrics_container = soup.select("div[data-lyrics-container='true']")
             
             if lyrics_container:
-                lyrics = lyrics_container.get_text(separator=" ").strip()
+                lyrics = " ".join([container.get_text(separator=" ").strip() for container in lyrics_container])
 
             else:
                 raise LyricsScraperException("Lyrics not found!")
-
             return lyrics
 
         except Exception: 
         
             raise LyricsScraperException(f"An error occurred while scraping the lyrics")
         
-
