a33e2ea19e0bacad999634b1d2b0cd77e642c3c7
ryhhill1998 <ryhhill1998@outlook.com>
2025-03-17 19:13:52 +0000

    added docs to lyrics route

--- lyrics_api/routers/lyrics.py
+++ lyrics_api/routers/lyrics.py
@@ -9,6 +9,31 @@ router = APIRouter()
 
 @router.post("/lyrics", response_model=LyricsResponse)
 async def get_lyrics(lyrics_request: LyricsRequest, data_service: DataServiceDependency) -> LyricsResponse:
+    """
+    Retrieves song lyrics based on the given request.
+
+    This endpoint fetches lyrics using the provided `DataServiceDependency` which manages the retrieval and storage of
+    lyrics.
+
+    Parameters
+    ----------
+    lyrics_request : LyricsRequest
+        The request object containing track ID, artist name, and track title.
+    data_service : DataServiceDependency
+        The data service dependency responsible for retrieving and storing lyrics.
+
+    Returns
+    -------
+    LyricsResponse
+        A structured response containing the track ID, artist name, track title, and lyrics.
+
+    Raises
+    ------
+    HTTPException
+        - 404 : If the lyrics cannot be found.
+        - 500 : If an unexpected error occurs during retrieval.
+    """
+
     try:
         lyrics = await data_service.get_lyrics(lyrics_request)
         return lyrics
