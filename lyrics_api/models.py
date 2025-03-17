df9830f898987fcbec5485eb08e32b2194d62677
ryhhill1998 <ryhhill1998@outlook.com>
2025-03-17 19:42:46 +0000

    added docs to models.py

--- lyrics_api/models.py
+++ lyrics_api/models.py
@@ -2,10 +2,34 @@ from pydantic import BaseModel
 
 
 class LyricsRequest(BaseModel):
+    """
+    Data model for a lyrics request.
+
+    Attributes
+    ----------
+    track_id : str
+        Unique identifier for the track.
+    artist_name : str
+        Name of the artist.
+    track_title : str
+        Title of the track.
+    """
+
     track_id: str
     artist_name: str
     track_title: str
 
 
 class LyricsResponse(LyricsRequest):
+    """
+    Data model for a lyrics response.
+
+    Inherits from LyricsRequest which provides the track_id, artist_name and track_title.
+
+    Attributes
+    ----------
+    lyrics : str
+        The lyrics of the requested track.
+    """
+
     lyrics: str
