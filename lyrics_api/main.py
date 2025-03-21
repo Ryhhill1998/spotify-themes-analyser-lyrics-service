1010726e56416fec0a834ecf274f27b3bb00b921
ryhhill1998 <ryhhill1998@outlook.com>
2025-03-21 16:45:05 +0000

    init logger in main lifespan, log every request and overrode global exc handler to return custom response and make sure to log

--- lyrics_api/main.py
+++ lyrics_api/main.py
@@ -1,9 +1,12 @@
 import asyncio
+import sys
 from contextlib import asynccontextmanager
 
 import aiosqlite
 import httpx
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from loguru import logger
 
 from lyrics_api.services.storage.storage_service import initialise_db
 from lyrics_api.settings import Settings
@@ -11,9 +14,17 @@ from lyrics_api.services.lyrics_scraper import LyricsScraper
 from lyrics_api.routers import lyrics
 
 
+def initialise_logger():
+    logger.remove()
+    logger.add(sys.stdout, format="{time} {level} {message}", level="INFO")
+    logger.add(sys.stderr, format="{time} {level} {message}", level="ERROR")
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     settings = Settings()
+
+    initialise_logger()
 
     # initialise database
     db = await aiosqlite.connect(settings.db_path)
@@ -41,3 +52,31 @@ def health_check():
 
 
 app.include_router(lyrics.router)
+
+
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, e: Exception):
+    """Handles all unhandled exceptions globally."""
+    logger.exception(f"Unhandled Exception occurred at {request.url} - {e}")
+
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "Something went wrong. Please try again later."},
+    )
+
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    """Middleware to log all incoming requests."""
+
+    ip_addr = request.client.host
+    port = request.client.port
+    url = request.url
+    req_method = request.method
+
+    log_message = f"{ip_addr}:{port} made {req_method} request to {url}."
+
+    logger.info(log_message)
+
+    response = await call_next(request)
+    return response
